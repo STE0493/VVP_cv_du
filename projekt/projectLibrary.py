994776748d96529fb6f02e79c93749f8276c7821
KaterinaStefankova <katerina.stefankova.st@vsb.cz>
2023-04-12 15:39:38 +0200

    project check naming

--- projekt/projectLibrary.py
+++ projekt/projectLibrary.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.cm
 from typing import Union
 import ipywidgets as widgets
 from ipywidgets import interact, interactive, fixed, interact_manual
